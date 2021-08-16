a27c14f0da5c3ff22c6942ad08b07b4226350b87
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-08-16 19:45:13 -0300

    desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany(
+  {},
+  { $set: { avaliacao: NumberInt("0") } },
+  { upsert: true },
+);
+
+db.produtos.updateMany(
+  { tags: { $all: ["bovino"] } },
+  { $inc: { avaliacao: 5 } },
+);
+
+db.produtos.updateMany({ tags: { $all: ["ave"] } }, { $inc: { avaliacao: 3 } });
+
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
