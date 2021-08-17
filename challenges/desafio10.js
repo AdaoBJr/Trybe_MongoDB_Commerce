af09894c1e4503322d88118923a98ef5a86a5a1d
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-17 20:44:06 -0300

    Challenge 7 to 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -4,7 +4,7 @@ db.produtos.updateOne({ nome: "Big Mac" }, { $set: { "vendasPorDia.3": 60 } });
 
 db.produtos
     .updateMany({
-        $and: [{ "vendasPorDia.tags": "bovino" }, { "vendasPorDia.tags": "pão" }] },
+        $and: [{ tags: "bovino" }, { tags: "pão" }] },
         { $set: { "vendasPorDia.6": 120 } });
 
 db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 }).pretty();
