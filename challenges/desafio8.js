c41cede7bbe85c8182667b0f24665420637729f0
renanpbraga <renanpbraga@gmail.com>
2021-08-17 16:25:18 -0300

    adiciona desafio 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,18 @@
+db.produtos.updateOne(
+  {
+    nome: "Quarteirão com Queijo",
+  },
+  {
+    $pop: {
+      ingredientes: -1,
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: false,
+    ingredientes: true,
+  },
+);
