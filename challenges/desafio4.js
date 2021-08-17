9a7554f1a15e81d4d93245a67047b108929ca4d4
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-08-17 14:45:13 -0300

    ajustando requisito 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -4,11 +4,7 @@ Para isso, escreva no arquivo desafio4.js quatro queries, nesta ordem: */
 // 1º Crie uma query que atribua a data corrente ao campo ultimaModificacao no sanduíche Big Mac. Para a data corrente faça uso do tipo Date.
 db.produtos.updateOne(
   { nome: "Big Mac" },
-  {
-    $currentDate: {
-      ultimaModificacao: { $type: "date" },
-    },
-  },
+  { $currentDate: { ultimaModificacao: { $type: "date" } } },
 );
 
 // 2º Crie uma query que retorne o nome de todos os documentos em que o campo ultimaModificacao existe.
