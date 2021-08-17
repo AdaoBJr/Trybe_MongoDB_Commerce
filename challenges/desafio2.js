23346922b5308f1e316dd26fd6c23d54df5e76f3
Adão Benites <adaobjr@gmail.com>
2021-08-17 13:50:24 -0400

    feat: :sparkles: Req_2

---
+++ challenges/desafio2.js
@@ -0,0 +1,7 @@
+// db.produtos.find();
+db.produtos.updateMany(
+  {},
+  { $set: { valorUnitario: NumberDecimal("0.00") } },
+  { upsert: true },
+);
+db.produtos.find({}, { valorUnitario: 1, nome: 1, _id: 0 });
