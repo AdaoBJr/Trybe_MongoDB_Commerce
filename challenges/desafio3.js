e6bdd7c40ed0b384936537bbc0d55992734ad579
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-08-17 16:05:18 -0300

    corrigindo 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -2,6 +2,6 @@ db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
 
 db.produtos.updateMany({ tags: { $all: ["bovino"] } }, { $set: { avaliacao: 5 } });
 
-db.produtos.updateMany({ tags: { $all: ["bovino"] } }, { $set: { avaliacao: 5 } });
+db.produtos.updateMany({ tags: { $all: ["ave"] } }, { $set: { avaliacao: 3 } });
 
 db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 });
