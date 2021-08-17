75f49e9ff348336b5c380f052f50724387d8266d
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-17 18:17:14 -0300

    Challenge 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,3 +1,3 @@
 db.produtos.updateMany({ nome: { $ne: "McChicken" } }, { $addToSet: { ingredientes: "Ketchup" } });
 
-db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
