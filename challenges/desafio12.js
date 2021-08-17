a1e4a5dbbe632a3864c02b4983af1ca4579c6d4c
root <diegoserafim1@gmail.com>
2021-08-17 17:55:28 -0300

    D12 - Ordene os elementos do array valoresNutricionais pelo campo percentual de forma descendente.

---
+++ challenges/desafio12.js
@@ -0,0 +1,14 @@
+// Ordene os elementos do array valoresNutricionais pelo campo percentual de forma descendente.
+
+db.produtos.updateMany(
+  {},
+  { $push: { valoresNutricionais: {
+    $each: [],
+    $sort: { percentual: -1 },
+  } } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, valoresNutricionais: 1, _id: 0 },
+).pretty();
---
+++ challenges/desafio13.js
@@ -0,0 +1,5 @@
+// Adiciona 'muito sódio' ao final do array 'tags' nos produtos em que o percentual de sódio seja maior ou igual a 40
+
+db.produtos.updateMany(
+  
+);
