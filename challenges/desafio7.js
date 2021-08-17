b45864a05ad5f873600447e5693028095ee3bada
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2021-08-17 14:57:24 -0300

    challeng 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,18 @@
+db.produtos.updateMany(
+    {},
+    {
+        $pull: {
+          ingredientes: "cebola",
+        },
+    },
+    { upsert: true },
+);
+  
+db.produtos.find(
+    {},
+    {
+        _id: 0,
+        nome: 1,
+        ingredientes: 1,
+    },
+);
