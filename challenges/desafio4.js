6a76788585ba2f059227a114f57e7adac15d936f
Aladino Borges <74330016+AladinoBorges@users.noreply.github.com>
2021-08-17 15:19:10 -0300

    Requisito 4: 100%.

---
+++ challenges/desafio4.js
@@ -0,0 +1,14 @@
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $currentDate: {
+      lastModified: true,
+      ultimaModificacao: { $type: "date" },
+    },
+  },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { _id: false, nome: true },
+);
