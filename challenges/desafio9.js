1ff20dfddc6547721212fde8a7cc249f7f5d5cc7
root <diegoserafim1@gmail.com>
2021-08-17 15:12:53 -0300

    D9 - Remove o último ingrediente do sanduíche Cheddar McMelt

---
+++ challenges/desafio9.js
@@ -0,0 +1,11 @@
+// Remove o último ingrediente do sanduíche Cheddar McMelt
+
+db.produtos.updateMany(
+  { nome: "Cheddar McMelt" },
+  { $pop: { ingredientes: 1 } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, ingredientes: 1, _id: 0 },
+).pretty();
