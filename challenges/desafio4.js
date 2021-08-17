01bc28508b36d83120b6c9634829fc47d4e09e35
Lara Capila <laracapilak@gmail.com>
2021-08-17 15:26:00 -0300

    Requirement 4 done

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,9 +1,8 @@
 db.produtos.updateOne(
   { nome: "Big Mac" },
-  { $currentDate: { ultimaModificacao: { $type: "date" } } },
+  { $currentDate: { ultimaModificacao: true } },
 );
 
-use("commerce");
 db.produtos.find(
   { ultimaModificacao: { $exists: true } },
   { nome: 1, _id: 0 },
