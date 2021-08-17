e55619097ef4a2ee1286d316891198accdc58312
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2021-08-17 11:27:14 -0300

    chaleng 3 and 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany(
+    { nome: "Big Mac" },
+    {
+      $currentDate: { ultimaModificacao: true },
+    },
+    { upsert: true },
+);
+  
+db.produtos.find(
+    {
+      ultimaModificacao: { $exists: true },
+    },
+    {
+        _id: 0,
+        nome: 1,
+    },
+);
