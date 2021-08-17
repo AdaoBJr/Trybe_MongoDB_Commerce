4b4ac57c05f11757642633d966464c2faa61d1d9
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-08-17 15:21:31 -0300

    desafios 9 e 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,17 +1,16 @@
-use("commerce");
 db.produtos.updateMany(
   {},
   {
-    $set: {"vendasPorDia": [0, 0, 0, 0, 0, 0, 0]}
-  }
+    $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] },
+  },
 );
 db.produtos.updateOne(
-  {nome: "Big Mac"},
+  { nome: "Big Mac" },
   {
     $inc: {
-      "vendasPorDia.3": 60
-    }
-  }
+      "vendasPorDia.3": 60,
+    },
+  },
 );
 db.produtos.updateMany(
   {
@@ -26,7 +25,5 @@ db.produtos.updateMany(
 
 db.produtos.find(
   {},
-  {nome:1, vendasPorDia: 1, _id: 0}  
+  { nome: 1, vendasPorDia: 1, _id: 0 },  
 );
-
-use("commerce");db.produtos.find();
