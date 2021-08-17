798efd11d0ff482d9f430724949ead5229537b92
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2021-08-17 14:45:34 -0300

    challeng 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,22 @@
+db.produtos.updateMany(
+    {
+      nome: {
+        $ne: "McChicken",
+      },
+    },
+    {
+        $addToSet: {
+          ingredientes: "ketchup",
+        },
+    },
+    { upsert: true },
+);
+  
+db.produtos.find(
+    {},
+    {
+        _id: 0,
+        nome: 1,
+        ingredientes: 1,
+    },
+);
