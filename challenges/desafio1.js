7d15dd149ea52b6f09da97a402a9b1bd8abf3488
Diegho <dieghonm@gmail.com>
2021-08-17 18:57:58 -0300

    Requisito 1, lint 2

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,7 +1,5 @@
 db.produtos.updateMany(
-  {}, { $set: { criadoPor: "Ronald McDonald" } }
-);
+  {}, { $set: { criadoPor: "Ronald McDonald" } });
 
 db.produtos.find(
-  {}, { nome: true, criadoPor: true, _id: false }
-);
+  {}, { nome: true, criadoPor: true, _id: false });
