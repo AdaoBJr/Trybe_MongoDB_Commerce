05785c73bd55112dc6a19ef6fbb950fbf553949e
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-08-17 14:20:46 -0300

    project started, requirements 1 , 2 , 3 , 4 done

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,10 +1,6 @@
 db.produtos.updateOne(
-  {
-    nome: "Big Mac", 
-  },
-  {
-    $currentDate: { ultimaModificacao: true }, 
-  }, 
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: true } }, 
 );
 
 db.produtos.find(
