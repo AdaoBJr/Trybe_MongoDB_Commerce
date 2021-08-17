e3f0baf7ce839623d8a3acfdd6cc74566b8ca55b
legalvao1 <legalvao1@gmail.com>
2021-08-17 10:08:46 -0300

    Desafio 2 - Adicionando o campo valor Unitario

---
+++ challenges/desafio2.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  {},
+  { $set: { 
+    valorUnitario: NumberDecimal("0.00"),
+    },
+  },
+  { $upsert: 1 },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, valorUnitario: 1, _id: 0 },
+);
