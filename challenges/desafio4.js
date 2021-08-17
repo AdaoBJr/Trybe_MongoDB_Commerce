56c7f41c9263638344f362ded2083fb4edfadead
Caio <caioqs.info@gmail.com>
2021-08-17 16:29:43 -0300

    Requisito 4 ESlint corrigido

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -4,4 +4,4 @@ db.produtos.updateOne(
   },
 );
 
-db.produtos.find({ultimaModificacao: { $exists: true }}, { _id: 0, nome: 1 });
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
