484c39538320a45658509edc3494dbcf4b58f838
Daniel <darkdanielr@gmail.com>
2021-08-17 17:54:02 -0300

    desafio 14 corrigido

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,9 +1,8 @@
 db.produtos.updateMany(
-  { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } },
+  { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } } },
   { 
-    $push: { tags: "muito sódio" },
+    $push: { tags: "contém sódio" },
   },
 );
 db.produtos.find({}, { _id: false, nome: true, tags: true });
 // https://docs.mongodb.com/manual/reference/operator/query/elemMatch/
-// https://app.betrybe.com/course/back-end/mongodb-updates-simples-e-complexos/updates-complexos-arrays-parte-2/4fd2d65a-b343-43a7-adb2-fc06227153f7/conteudos/02fbc3d8-a067-4634-950d-bdd4e84b9bfc/operador-elemmatch/e543c1cc-d430-445d-a655-23a5c12211eb?use_case=side_bar
