26ef3c0558547e1dd31780de32cdba6f2c1d18b9
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-08-17 16:56:38 -0300

    desafio 12

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -6,4 +6,4 @@ db.produtos.update({ nome: "Big Mac" },
 db.produtos.update({ tags: { $all: ["bovino", "pão"] } }, 
 { $inc: { "vendasPorDia.6": 120 } });
 
-db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
+db.produtos.find({ }, { nome: 1, vendasPorDia: 1, _id: 0 });
