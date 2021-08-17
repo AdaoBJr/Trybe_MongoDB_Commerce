599ce554b3916a86372a635b6086661d4fb1b860
root <diegoserafim1@gmail.com>
2021-08-17 18:52:30 -0300

    D18 - Quantos documentos contêm a expressão 'feito com' utilizando o operador

---
+++ challenges/desafio18.js
@@ -0,0 +1,10 @@
+// Quantos documentos contêm a expressão 'feito com' utilizando o operador $text
+
+db.produtos.createIndex(
+  { descricao: "text" },
+  { default_language: "portuguese" },
+);
+
+db.produtos.find(
+  { $text: { $search: "feito com" } },
+).count();
