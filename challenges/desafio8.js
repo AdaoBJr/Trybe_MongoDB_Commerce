eeefe944f239afde1c6b641eee3180c461862a66
riquee <strikedv@hotmail.com>
2021-08-17 13:29:06 -0300

    req8 finalizado

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,3 +1,3 @@
-db.produtos.update({ nome: 'Quarteirão com Queijo' }, { $pop: { ingredientes: -1 } });
+db.produtos.update({ nome: "Quarteirão com Queijo" }, { $pop: { ingredientes: -1 } });
 
 db.produtos.find({}, { _id: 0, ingredientes: 1, nome: 1 });
