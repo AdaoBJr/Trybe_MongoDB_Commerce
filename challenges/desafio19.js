c821681ef65f6995931217b734fb8d34e8a600b7
Paulo Xavier <pxavierf@gmail.com>
2021-08-17 14:27:25 -0300

    fixing desafio19

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,3 +1,3 @@
 db.produtos.updateMany({}, { $rename: { descricao: descricaoSite } });
 
-db.produtos.find({}, { nome: 1, descricao: 1, descricadescricaoSite: 1, _id: 0 });
+db.produtos.find({}, { nome: 1, descricao: 1, descricaoSite: 1, _id: 0 });
