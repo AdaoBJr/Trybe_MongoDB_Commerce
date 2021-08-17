ff77b19680ea7efe0a195134f45d103d0382da4b
root <diegoserafim1@gmail.com>
2021-08-17 12:59:58 -0300

    D1 - Inclui o campo criadoPor em todos os documentos, colocando 'Ronald McDonald' no valor desse campo

---
+++ challenges/banco.sh
@@ -0,0 +1,8 @@
+DBNAME=commerce ./scripts/resetdb.sh assets/produtos
+
+# rodar o teste(tudo)
+./scripts/evaluate.sh
+
+# roda 1 só teste
+./scripts/evaluate.sh desafio1
+
---
+++ challenges/desafio1.js
@@ -0,0 +1,11 @@
+// Inclui o campo criadoPor em todos os documentos, colocando "Ronald McDonald" no valor desse campo.
+
+db.produtos.updateMany(
+  {},
+  { $set: { criadoPor: "Ronald McDonald" } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, criadoPor: 1, _id: 0 },
+).pretty();
