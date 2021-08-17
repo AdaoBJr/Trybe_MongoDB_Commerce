3a1c177b0c4cfa42951dcbaee970b5733a57b041
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-08-17 14:29:38 -0300

    fix: requisito 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -6,7 +6,7 @@ db.produtos.updateMany(
 );
 db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 db.produtos.updateOne(
-  { $and: [{ tags: "bovino" }, { tags: "pão" }] }, 
+  { tags: { $all: ["bovino", "pão"] } }, 
   { $inc: { "vendasPorDia.6": 120 } },
 );
 db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
