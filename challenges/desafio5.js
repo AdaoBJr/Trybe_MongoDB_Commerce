f9c00b9694c88371956ad01fa44e81e8208c4bca
CamilaCode7 <CamilaCode7>
2021-08-17 16:18:45 -0300

    adicionando req 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany(
+  { nome: { $in: ["Big Mac", "Quarteirão com Queijo", "Cheddar McMelt", "Extra Chicken"] } },
+  { $addToSet: {
+    ingredientes: "Ketchup",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, ingredientes: 1, _id: 0 },
+);
