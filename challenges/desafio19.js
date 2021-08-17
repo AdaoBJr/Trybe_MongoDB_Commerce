afc7239173559c043df2e93f4493a0b77203a76e
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-08-17 11:11:50 -0300

    Renomeando o campo descricao para descricaoSite em todos os documentos.

---
+++ challenges/desafio19.js
@@ -0,0 +1,18 @@
+/* 19 - Renomeie o campo descricao para descricaoSite em todos os documentos.
+Para isso, escreva no arquivo desafio19.js duas queries, nesta ordem: */
+
+// 1º Crie uma query que faça a renomeação do campo descricao para descricaoSite em todos os documentos.
+db.produtos.updateMany(
+  {},
+  {
+    $rename: {
+      descricao: "descricaoSite",
+    },
+  },
+);
+
+// 2º Crie uma query que retorne o nome, descricao e descricaoSite de todos os documentos.
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, descricao: 1, descricaoSite: 1 },
+);
