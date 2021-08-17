282a22d678932884b9e5094527bfea56c2b1abe9
root <diegoserafim1@gmail.com>
2021-08-17 19:11:11 -0300

    D20 - Remove o campo curtidas do item Big Mac

---
+++ challenges/desafio20.js
@@ -0,0 +1,11 @@
+// Remove o campo curtidas do item Big Mac
+
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $unset: { curtidas: "" } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, curtidas: 1, _id: 0 },
+).pretty();
