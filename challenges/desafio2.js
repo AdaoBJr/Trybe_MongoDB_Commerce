4b58078ab064ad8e680785ca487545e55ff788b7
Roger Franco <rogerfrancogalt@gmail.com>
2021-08-17 14:59:42 -0400

    requisitos 1-2

---
+++ challenges/desafio2.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany(
+  { valorUnitario: { $exists: false } },
+  {
+    $push: {
+      valorUnitario: NumberDecimal("0.00") 
+    }
+  }
+);
+
+db.produtos.find({},{_id:0, nome: 1, valorUnitario:1});
