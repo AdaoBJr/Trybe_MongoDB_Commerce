acf3011bec633cea0836ba474c10bd0b84e1d3ca
Diegho <dieghonm@gmail.com>
2021-08-18 01:54:19 -0300

    Requisito 5 a 9

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,5 +1,3 @@
-// Crie uma query que faça a inclusão de bacon no final da lista de ingredientes 
-// dos sanduíches Big Mac e Quarteirão com Queijo.
 db.produtos.updateMany(
   { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
   { $push: { ingredientes: bacon } },
--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,9 +1,7 @@
-const { RuleTester } = require("eslint");
-
 db.produtos.updadeOne(
   { name: "Quarteirão com Queijo" }, { $pop: { item: -1 } },
 );
 
 db.produtos.find(
-  {}, { nome: true, ingredientes: RuleTester, _id: false }
+  {}, { nome: true, ingredientes: true, _id: false },
 );
