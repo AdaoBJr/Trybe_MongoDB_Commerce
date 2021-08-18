c1cea61bcd0c96dc81d4a3dd86a5c9d4fb7e4430
Diegho <dieghonm@gmail.com>
2021-08-18 01:36:10 -0300

    Requisito 5 e 6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany(
-  { nome: { $ne: "McChicken" } }, { $addToSet: { ingredientes: ketchup } },
+  { nome: { $ne: "McChicken" } }, { $addToSet: { ingredientes: "ketchup" } },
 );
 
 db.produtos.find(
---
+++ challenges/desafio6.js
@@ -0,0 +1,10 @@
+// Crie uma query que faça a inclusão de bacon no final da lista de ingredientes 
+// dos sanduíches Big Mac e Quarteirão com Queijo.
+db.produtos.updateMany(
+  { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+  { $push: { ingredientes: "bacon" } },
+);
+
+db.produtos.find(
+  { nome: true, ingredientes: true, _id: false },
+);
