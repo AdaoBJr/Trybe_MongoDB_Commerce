fb2c4ba927ab416f4a38ca88215593026e2d05e6
Nícolas Tozato <ntozato9@gmail.com>
2021-08-18 20:06:56 -0300

    lint

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,11 +1,11 @@
 db.produtos.updateOne(
   { nome: "Big Mac" },
   {
-    $currentDate: { ultimaModificacao: true }
-  }
+    $currentDate: { ultimaModificacao: true },
+  },
 );
 
 db.produtos.find(
   { ultimaModificacao: { $exists: true } },
-  { _id: 0, nome: 1 }
+  { _id: 0, nome: 1 },
 );
