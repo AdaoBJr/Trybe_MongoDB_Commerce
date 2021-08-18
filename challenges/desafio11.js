d6e901c7bfd4a00a74ff848ada99455248dd797e
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-08-18 00:45:17 -0300

    Challenge 11 to 16

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,6 +1,6 @@
 db.produtos
     .updateMany({}, {
-        $push: { tags: { $each: ["combo", "tasty"], $sort: { tags: 1 } } } },
-        { collection: { locale: "pt" } });
+        $push: { tags: { $each: ["combo", "tasty"], $sort: 1 } } },
+        { collation: { locale: "pt" } });
 
 db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
