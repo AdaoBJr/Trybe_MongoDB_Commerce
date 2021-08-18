ee354f7af6697a197d9d43cedb72970909aa4d4d
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-08-18 16:06:35 -0300

    desafio13

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,3 +1,4 @@
-db.produtos.updateMany({ nome: "Big Mac" }, { $inc: { "vendasPorDia.2": 60 } });
-db.produtos.updateMany({ tags: { $in: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.5": 120 } });
+db.produtos.updateMany({}, { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } });
+db.produtos.updateMany({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
+db.produtos.updateMany({ tags: { $in: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.6": 120 } });
 db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
