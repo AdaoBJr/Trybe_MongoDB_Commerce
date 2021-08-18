3da15bcf318a3d9e41dc816aec90334f90cee527
Breno3B <brenorm@gmail.com>
2021-08-18 03:13:34 -0300

    20º requirement

---
+++ challenges/desafio20.js
@@ -0,0 +1,10 @@
+// Crie uma query que faça a remoção do campo curtidas do item Big Mac.
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $unset: { curtidas: "" } },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, curtidas: 1 },
+);
