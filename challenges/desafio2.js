38e259c77d2077f96ec80f42163c5ff3fea1d0a6
Jonathan Souza <souzajonathan570@gmail.com>
2021-08-18 08:07:46 -0300

    desafio3 concluido

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -2,8 +2,8 @@ db.produtos.updateMany(
   {},
   {
     $set: {
-      valorUnitario: NumberDecimal("0.00")
-    }
+      valorUnitario: NumberDecimal("0.00"),
+    },
   },
 );
 
@@ -13,5 +13,5 @@ db.produtos.find(
     _id: 0,
     nome: 1,
     valorUnitario: 1,
-  }
+  },
 );
