0b48cecb48e3b839d4ffeac14eb2fa5d21c0d251
Johnny-batata <you@example.com>
2021-08-18 03:09:10 -0300

    requirement 11 fixed

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,4 +1,4 @@
 db.produtos.updateMany({}, { $push: {
-  tags: { $each: ["combo", "tasty"], $sort: { tags: 1 } },
+  tags: { $each: ["combo", "tasty"], $sort: 1 },
 } });
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
