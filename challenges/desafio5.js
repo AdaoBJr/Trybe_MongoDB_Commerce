7d27834cb231cf30b14d1c6148b6a5dfdaea52d2
brunobastosfer <brunobastos@edu.unifor.br>
2021-08-17 22:15:46 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+  { 
+    $and: [
+    { nome: { $ne: "McChicken" } },
+    { ingredientes: { $not: { $regex: "ketchup" } } },
+  ],
+},
+  {
+    $push: { ingredientes: "ketchup" },
+  },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, ingredientes: 1 },
+);
