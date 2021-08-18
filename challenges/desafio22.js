9a6bc299ee9de50c6025a0029a7cd26283c56ebc
Emerson Saturnino <programador.emerson@hotmail.com>
2021-08-17 23:09:13 -0300

    Desafio 22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,9 +1,4 @@
-db.produtos.updateMany(
-  { valorUnitario: { $exists: false } },
-  { $set: { valorUnitario: NumberDecimal("0.00") } },
-);
-
 db.produtos.find(
-  {},
-  { nome: 1, valorUnitario: 1, _id: 0 },
+  { vendidos: { $mod: [5, 0] } },
+  { nome: 1, vendidos: 1, _id: 0 },
 );
