6f10e9afb77dc5b46f9231911e96ae7a9db48e37
Johnny-batata <you@example.com>
2021-08-18 03:49:17 -0300

    pushing again

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,4 +1,5 @@
 db.produtos.updateMany({ 
   nome: "Big Mac" }, { $currentDate: { ultimaModificacao: { $type: "date" } } });
 
-  db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
+  db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, 
+    nome: 1 });
