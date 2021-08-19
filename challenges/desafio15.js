987055a769d8d74724d0fdbde2f0099b2fbc27c0
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-08-19 01:45:46 -0300

    Desafio 15

---
+++ challenges/desafio15.js
@@ -0,0 +1,10 @@
+/*
+15 - Conte quantos produtos contêm Mc no nome, sem considerar letras maiúsculas ou minúsculas.
+*/
+db.produtos.find(
+  { nome: { $regex: /.*mc.*/i } },
+  {
+    _id: 0,
+    $inc: 1,
+  },
+).count();
