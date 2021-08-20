5a433958c91ab718a6056f8495cc2783dad39022
Aurelio Scarante <aurelioscarante25@gmail.com>
2021-08-20 00:10:50 -0300

    Desafio 5 :beers:

---
+++ challenges/desafio5.js
@@ -0,0 +1,25 @@
+// Desafio 5
+
+db.produtos.updateMany(
+  {
+    nome:
+    {
+      $ne: "McChicken",
+    },
+  },
+  {
+    $addToSet:
+      {
+        ingredientes: "ketchup",
+      },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    ingredientes: 1,
+  },
+);
