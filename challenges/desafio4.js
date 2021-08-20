176204b93c97a270f5f3b774f45716dad4487597
Aurelio Scarante <aurelioscarante25@gmail.com>
2021-08-20 00:00:08 -0300

    Desafio 4 :beers:

---
+++ challenges/desafio4.js
@@ -0,0 +1,24 @@
+// Desafio 4
+
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  {
+    $set:
+      {
+        ultimaModificacao: Date(),
+      },
+  },
+);
+
+db.produtos.find(
+  {
+    ultimaModificacao:
+      {
+        $exists: true,
+      },
+  },
+  {
+    _id: 0,
+    nome: 1,
+  },
+);
