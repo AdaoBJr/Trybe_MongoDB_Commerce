77cb39cfa95c9c28355c35900310339dd70fe4ae
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-08-19 23:21:55 -0300

    Adicionando requisito 12 a 18

---
+++ challenges/desafio13.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany({ "valoresNutricionais.2.percentual": { $gte: 40 } },
+  {
+    $push: {
+      tags: {
+        $each: ["muito sódio"],
+      },
+    },
+});
+
+db.produtos.find({}, { _id: 0, tags: 1, nome: 1 });
