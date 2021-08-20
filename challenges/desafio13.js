2072602f16e92ef01a6a67b324c6c48809173f2c
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-08-20 09:31:53 -0300

    feat: :sparkles: partial requirement 13.2 complete

---
+++ challenges/desafio13.js
@@ -0,0 +1,17 @@
+/*
+  13 - Adicione o elemento muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40.
+
+  13.1 - Crie uma query que faça a adição do elemento muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40.
+*/
+
+/*
+  13.2 - Crie uma query que retorne o nome e tags de todos os documentos.
+*/
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    tags: 1,
+  },
+);
