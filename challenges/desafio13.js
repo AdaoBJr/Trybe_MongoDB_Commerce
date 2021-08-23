ff4dda9216397118a5b7b1dadfc82ac0a0f7ef6f
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-08-23 08:13:16 -0300

    feat: :sparkles: partial requirement 13.1 and requirement 13 complete

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -3,6 +3,25 @@
 
   13.1 - Crie uma query que faça a adição do elemento muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40.
 */
+/*
+  Material consultado sobre arrays que contenham subdocumentos e se deseja especificar vários campos desses subdocumentos como filtro:
+  https://docs.mongodb.com/manual/tutorial/query-array-of-documents/#a-single-nested-document-meets-multiple-query-conditions-on-nested-fields
+*/
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        percentual: { $gte: 40 },
+      },
+    },
+  },
+  {
+    $push: {
+      tags: "muito sódio",
+    },
+  },
+);
 
 /*
   13.2 - Crie uma query que retorne o nome e tags de todos os documentos.
