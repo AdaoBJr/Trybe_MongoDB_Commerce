88923dfac1df00704b45e00a685920f1907edb19
palenske <viniciuspalenske@gmail.com>
2021-08-23 13:55:25 -0300

    entrega desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany(
+  {},
+  {
+    $addToSet: {
+      tags: {
+        $each: ["combo", "tasty"],
+      },
+    },
+  },
+  {
+    sort: {
+      tags: 1,
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, tags: 1 },
+);
