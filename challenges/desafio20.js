5bc85fd6b4b4eacad3711a0ffab35fdafa9c944e
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-08-24 00:43:07 -0300

    feat: :sparkles: partial requirement 20.1 and requirement 20 complete

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -3,6 +3,16 @@
 
   20.1 - Crie uma query que faça a remoção do campo curtidas do item Big Mac.
 */
+db.produtos.updateOne(
+  {
+    nome: "Big Mac",
+  },
+  {
+    $unset: {
+      curtidas: "",
+    },
+  },
+);
 
 /*
   20.2 - Crie uma query que retorne o nome e curtidas de todos os documentos.
