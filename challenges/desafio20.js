26d455d073177b93080388e91b1a13b1cf6ca2b4
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-08-24 00:39:19 -0300

    feat: :sparkles: partial requirement 20.2

---
+++ challenges/desafio20.js
@@ -0,0 +1,17 @@
+/*
+  20 - Remova o campo curtidas do item Big Mac.
+
+  20.1 - Crie uma query que faça a remoção do campo curtidas do item Big Mac.
+*/
+
+/*
+  20.2 - Crie uma query que retorne o nome e curtidas de todos os documentos.
+*/
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    curtidas: 1,
+  },
+);
