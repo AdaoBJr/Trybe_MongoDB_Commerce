a9274f6afcdcbab1ec540da26ea571f4fa6c233d
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-01 10:46:32 -0300

    Requisito 18

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -9,3 +9,8 @@ db.produtos.count(
     },
   },
 );
+
+// referências:
+// https://docs.mongodb.com/manual/reference/method/db.collection.createIndex/#examples
+// https://docs.mongodb.com/manual/reference/text-search-languages/#std-label-text-search-languages
+// https://docs.mongodb.com/manual/tutorial/specify-language-for-text-index/#std-label-specify-language-field-text-index-example
