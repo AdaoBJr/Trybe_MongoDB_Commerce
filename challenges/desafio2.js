1428051d76bbe42929e6a67b12e11cd17c9a1399
Malbathan <malbathanlima@gmail.com>
2021-09-07 12:33:38 -0300

    req 02 - refactored

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -2,5 +2,5 @@ db.produtos.updateMany({
     valorUnitaio: { $exists: false },
   }, 
   { $set: { valorUnitario: 0.00 } });
+  { $set: { valorUnitario: NumberDecimal("0.00") } });
   db.produtos.find({}, { _id: 0, valorUnitario: 1, nome: 1 });
-  
