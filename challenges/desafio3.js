d4a69e51a7b8f2660447b6926b8d610c4b9c9948
Malbathan <malbathanlima@gmail.com>
2021-09-07 12:44:01 -0300

    lint

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -3,4 +3,3 @@ db.produtos.updateMany({},
     db.produtos.updateMany({ tags: { $all: ["bovino"] } }, { $inc: { avaliacao: 5 } });
     db.produtos.updateMany({ tags: { $all: ["ave"] } }, { $inc: { avaliacao: 3 } });
     db.produtos.find({}, { _id: 0, avaliacao: 1, nome: 1 });
-    
