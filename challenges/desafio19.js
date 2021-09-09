048c5d4c57678a9aa7cb9d5088114c4d537b713e
Junior030 <juniorhenrique030@gmail.com>
2021-09-09 19:47:59 -0300

    Desafios 17, 18, 19 e 20

---
+++ challenges/desafio19.js
@@ -0,0 +1,2 @@
+db.foo.updateMany({}, { $rename: { descricao: descricaoSite } });
+db.produtos.find({}, { _id: 0, nome: 1, descricao: 1, descricaoSite: 1 });
---
+++ challenges/desafio20.js
@@ -0,0 +1,2 @@
+db.foo.updateOne({ nome: "Big Mac" }, { $unset: { curtidas: "" } });
+db.produtos.find({}, { _id: 0, nome: 1, curtidas: 1 });
