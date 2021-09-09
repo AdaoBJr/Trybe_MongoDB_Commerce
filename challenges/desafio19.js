d6e98e5b8897eb6f868a69c2d78d95ad068c06b3
Junior030 <juniorhenrique030@gmail.com>
2021-09-09 19:53:16 -0300

    Desafios 19 and 20 erros

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,2 +1,2 @@
-db.foo.updateMany({}, { $rename: { descricao: descricaoSite } });
+db.foo.updateMany({}, { $rename: { descricao: "descricaoSite" } });
 db.produtos.find({}, { _id: 0, nome: 1, descricao: 1, descricaoSite: 1 });
